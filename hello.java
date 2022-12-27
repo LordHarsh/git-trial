2236005b25101bd2f741c0c6aa061af697e70661
Harsh Banka <56753150+LordHarsh@users.noreply.github.com>
2022-12-27 19:22:39 +0000

    added java

---
+++ hello.java
@@ -0,0 +1,8 @@
+class hello{
+    public static void main(String agrs[]) {
+        if(agrs.length >= 1)
+            System.out.println("Hello! "+ agrs[0]);
+        else
+            System.out.println("Hello World");
+    }
+}
